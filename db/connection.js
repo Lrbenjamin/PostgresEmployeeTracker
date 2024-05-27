ce11378499ea4c5343b7473398ee54ef692ecb05
LBenjamin <Lrbenjamin22@gmail.com>
2024-05-27 13:40:23 -0700

    Finished Connections with comments

--- db/connection.js
+++ db/connection.js
@@ -1,15 +1,15 @@
-const { Client } = require('pg');
+const { Client } = require('pg'); // Import the pg module which is a PostgreSQL client for Node.js
 
-const connectDb = () => {
-  const client = new Client({
+const connectDb = () => {   // Define a function to connect to the PostgreSQL database
+  const client = new Client({  // Create a new instance of the Client with the database connection details
     user: 'postgres',
     host: 'localhost',
     database: 'EmployeeDatabase',
-    password: '',
+    password: 'Lb177496',
     port: 5432,
   });
 
-  client.connect(err => {
+  client.connect(err => { // Connect to the database
     if (err) {
       console.error('Connection error', err.stack);
     } else {
@@ -17,7 +17,7 @@ const connectDb = () => {
     }
   });
 
-  return client;
+  return client; // Return the client instance to allow further operations on the database
 };
 
-module.exports = connectDb;
+module.exports = connectDb; // Export the connectDb function so it can be used in other modules
